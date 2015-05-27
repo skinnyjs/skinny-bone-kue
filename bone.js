3b91b4a46eb3744400272c49bae48b4f18da0e27
Ivan Shumkov <ivan@shumkov.ru>
2015-05-27 19:08:14 +0300

    add bone

---
+++ bone.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const bluebird = require('bluebird');
+const kue = require('kue');
+
+module.exports = function attachKue(skinny, options) {
+    skinny.on('*initialize', function *initializeKue() {
+        skinny.kue = kue.createQueue(options);
+
+        skinny.kue.on('error', function(error) {
+            skinny.emit('warning', error);
+        });
+
+        bluebird.promisifyAll(skinny.kue);
+    });
+
+    skinny.on('*shutdown', function *shutdownKue() {
+        if (skinny.kue) {
+            yield skinny.kue.shutdownAsync();
+        }
+    });
+};
